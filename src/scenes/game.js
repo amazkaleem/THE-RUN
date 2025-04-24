4dfb0adc580675d83e5e30249a8b38710b781849
amazkaleem <amazkaleem8@gmail.com>
2025-04-24 07:10:50 +0500

    prototyping2

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -21,7 +21,8 @@ export default function game() {
     k.add([k.sprite("platforms"), k.pos(384, 450), k.scale(4)]),
   ];
 
-  const sonic = makeSonic(k.vec2(200, 745));
+
+  const sonic = makeSonic(k.vec2(300, 745));
   sonic.setControls();
   sonic.setEvents();
 
@@ -39,13 +40,33 @@ export default function game() {
     dismissControlsAction.cancel();
   });
 
-  const scoreText = k.add([
-    k.text("SCORE : 0", { font: "mania", size: 72 }),
+  let miles = 31;
+
+  // Adding color to the miles text (blue)
+  const milesText = k.add([
+    k.text(`MILES : ${miles}`, {
+        font: "mania",
+        size: 72,
+    }),
+    k.color(0, 100, 255), // Blue color
+    k.pos(20, 80),
+    k.anchor("topleft"),
+  ]);
+
+  // Adding color to the BMI text (green for "HEALTHY")
+  const bmiText = k.add([
+    k.text("BMI LEVEL : HEALTHY", {
+        font: "mania",
+        size: 72,
+    }),
+    k.color(0, 200, 0), // Green color for "HEALTHY"
     k.pos(20, 20),
+    k.anchor("topleft"),
   ]);
 
   let score = 0;
   let scoreMultiplier = 0;
+  let knockbackDirection = 1;
   
 
   sonic.onCollide("enemy", (enemy) => {
@@ -62,7 +83,6 @@ export default function game() {
         sonic.jump();
         scoreMultiplier += 1;
         score += 10 * scoreMultiplier;
-        scoreText.text = `SCORE : ${score}`;
         if (scoreMultiplier === 1)
           sonic.ringCollectUI.text = `+${10 * scoreMultiplier}`;
         if (scoreMultiplier > 1) sonic.ringCollectUI.text = `x${scoreMultiplier}`;
@@ -79,12 +99,14 @@ export default function game() {
     // Make sonic invincible and transparent
     sonic.isInvincible = true;
     sonic.opacity = 0.5;
+    sonic.fatCounter -= 1;
+    console.log(sonic.fatCounter);
     
     // Knockback effect
-    let knockbackDirection = -1;
+    knockbackDirection = -1;
     sonic.tween(
       sonic.pos.x,
-      sonic.pos.x + (knockbackDirection * 50),
+      sonic.pos.x + (knockbackDirection * 100),
       0.75,
       (value) => sonic.pos.x = value
     );
@@ -98,9 +120,11 @@ export default function game() {
         sonic.opacity = 0.5;
         sonic.isInvincible = true;
         knockbackDirection = 1;
+        sonic.fatCounter += 1;
+        console.log(sonic.fatCounter);
         sonic.tween(
           sonic.pos.x,
-          sonic.pos.x + (knockbackDirection * 50),
+          sonic.pos.x + (knockbackDirection * 100),
           0.75,
           (value) => sonic.pos.x = value
         );
@@ -111,12 +135,22 @@ export default function game() {
         });
       });
     });
+
   });
 
 
   let gameSpeed = 300;
+
   k.loop(1, () => {
-    gameSpeed += 25;
+    gameSpeed += 40;
+  });
+
+  // Set up a timed event to decrease miles every 10 seconds
+  k.loop(10, () => {
+    if (miles > 0) {
+        miles -= 1;
+        milesText.text = `MILES : ${miles}`;
+    }
   });
 
   const spawnMotoBug = () => {
@@ -149,27 +183,41 @@ export default function game() {
     "platform",
   ]);
 
-  k.onUpdate(() => {
-    if (sonic.isGrounded()) scoreMultiplier = 0;
 
-    if (bgPieces[1].pos.x < 0) {
-      bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
-      bgPieces.push(bgPieces.shift());
-    }
+// onUpdate() for checking all conditions every frame
+k.onUpdate(() => {
+  if (sonic.isGrounded()) scoreMultiplier = 0;
+  
+  if (sonic.fatCounter === 2) {
+    bmiText.text = `BMI LEVEL: FAT`
+    bmiText.color = k.rgb(255, 255, 0);
+  } else if (sonic.fatCounter === 1) {
+    bmiText.text = `BMI LEVEL: OBESE`
+    bmiText.color = k.rgb(255, 0, 0);
+  } else if (sonic.fatCounter == 0) {
+    k.go("gameover", citySfx);
+  }
+  
+  if (bgPieces[1].pos.x < 0) {
+    bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
+    bgPieces.push(bgPieces.shift());
+  }
+  
+  bgPieces[0].move(-100, 0);
+  bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+  
+  // for jump effect
+  bgPieces[0].moveTo(bgPieces[0].pos.x, -sonic.pos.y / 10 - 50);
+  bgPieces[1].moveTo(bgPieces[1].pos.x, -sonic.pos.y / 10 - 50);
+  
+  if (platforms[1].pos.x < 0) {
+    platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
+    platforms.push(platforms.shift());
+  }
+  
+  platforms[0].move(-gameSpeed, 0);
+  platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
 
-    bgPieces[0].move(-100, 0);
-    bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+});
 
-    // for jump effect
-    bgPieces[0].moveTo(bgPieces[0].pos.x, -sonic.pos.y / 10 - 50);
-    bgPieces[1].moveTo(bgPieces[1].pos.x, -sonic.pos.y / 10 - 50);
-
-    if (platforms[1].pos.x < 0) {
-      platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
-      platforms.push(platforms.shift());
-    }
-
-    platforms[0].move(-gameSpeed, 0);
-    platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
-  });
 }
